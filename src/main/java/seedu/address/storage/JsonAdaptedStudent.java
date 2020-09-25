0961f351d06a4d2f0c8427f539325d1aa3d77e9b
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2020-09-25 21:33:46 +0800

    Rectify import checkstyle error

--- src/main/java/seedu/address/storage/JsonAdaptedStudent.java
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
