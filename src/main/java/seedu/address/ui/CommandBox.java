8c40bc9b7cb7081899dccb849d24f011064d6de4
samlsm <samlsm2803@gmail.com>
2020-10-01 11:05:00 +0800

    Apply suggestions from code review
    Co-authored-by: ypinhsuan <58677983+ypinhsuan@users.noreply.github.com>

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -28,7 +28,7 @@ public class CommandBox extends UiPart<Region> {
         super(FXML);
         this.commandExecutor = commandExecutor;
 
-        // calls #setStyleToDefault() whenever there is a change to the text of the command box.
+        // Calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
 
