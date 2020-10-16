08bc64237b5c3f55bb863442a6cc0e68ca52e751
Dexter Tan <dev@dextertanyj.com>
2020-10-16 17:04:34 +0800

    Add integration tests

--- src/test/java/seedu/address/model/lesson/LessonTest.java
+++ src/test/java/seedu/address/model/lesson/LessonTest.java
@@ -1,23 +1,67 @@
 package seedu.address.model.lesson;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DAY_WED_LESSON_WED_2_TO_4;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_END_TIME_1600_LESSON_WED_2_TO_4;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NUMBER_OF_OCCURRENCES_7_LESSON_WED_2_TO_4;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PARTICIPATION_SCORE_33;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_START_TIME_1400_LESSON_WED_2_TO_4;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_UUID_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_VENUE_COM1_B111_LESSON_WED_2_TO_4;
 import static seedu.address.model.lesson.Lesson.TIME_FORMATTER;
 import static seedu.address.testutil.TypicalLesson.LESSON_FRI_8_TO_10;
 import static seedu.address.testutil.TypicalLesson.LESSON_WED_2_TO_4;
 
 import java.time.LocalTime;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.attendance.Attendance;
+import seedu.address.model.attendance.AttendanceRecord;
+import seedu.address.model.attendance.AttendanceRecordList;
 import seedu.address.testutil.LessonBuilder;
 
 public class LessonTest {
+
+    private static final LocalTime VALID_START_TIME = LocalTime.parse(VALID_START_TIME_1400_LESSON_WED_2_TO_4);
+    private static final LocalTime VALID_END_TIME = LocalTime.parse(VALID_END_TIME_1600_LESSON_WED_2_TO_4);
+    private static final Venue VALID_VENUE = new Venue(VALID_VENUE_COM1_B111_LESSON_WED_2_TO_4);
+    private static final NumberOfOccurrences VALID_NUMBER_OF_OCCURRENCES =
+            new NumberOfOccurrences(1);
+    private static final Attendance VALID_ATTENDANCE = new Attendance(VALID_PARTICIPATION_SCORE_33);
+    private static final AttendanceRecord VALID_ATTENDANCE_RECORD =
+            new AttendanceRecord(Map.of(UUID.fromString(VALID_UUID_AMY), VALID_ATTENDANCE));
+    private static final AttendanceRecordList VALID_ATTENDANCE_RECORD_LIST =
+            new AttendanceRecordList(Collections.singletonList(VALID_ATTENDANCE_RECORD));
+
+    @Test
+    public void constructor_newLesson_hasNewAttendanceRecordList() {
+        Lesson lesson = new Lesson(VALID_START_TIME, VALID_END_TIME,
+                VALID_DAY_WED_LESSON_WED_2_TO_4, VALID_NUMBER_OF_OCCURRENCES, VALID_VENUE);
+        assertEquals(VALID_START_TIME, lesson.getStartTime());
+        assertEquals(VALID_END_TIME, lesson.getEndTime());
+        assertEquals(VALID_DAY_WED_LESSON_WED_2_TO_4, lesson.getDay());
+        assertEquals(VALID_NUMBER_OF_OCCURRENCES, lesson.getNumberOfOccurrences());
+        assertEquals(VALID_VENUE, lesson.getVenue());
+        assertEquals(new AttendanceRecordList(1), lesson.getAttendanceRecordList());
+    }
+
+    @Test
+    public void constructor_existingAttendanceRecordList_hasCorrectAttendanceRecordList() {
+        Lesson lesson = new Lesson(VALID_START_TIME, VALID_END_TIME, VALID_DAY_WED_LESSON_WED_2_TO_4,
+                VALID_NUMBER_OF_OCCURRENCES, VALID_VENUE, VALID_ATTENDANCE_RECORD_LIST);
+        assertEquals(VALID_START_TIME, lesson.getStartTime());
+        assertEquals(VALID_END_TIME, lesson.getEndTime());
+        assertEquals(VALID_DAY_WED_LESSON_WED_2_TO_4, lesson.getDay());
+        assertEquals(VALID_NUMBER_OF_OCCURRENCES, lesson.getNumberOfOccurrences());
+        assertEquals(VALID_VENUE, lesson.getVenue());
+        assertEquals(VALID_ATTENDANCE_RECORD_LIST, lesson.getAttendanceRecordList());
+    }
 
     @Test
     public void isSameLesson() {
@@ -53,6 +97,28 @@ public class LessonTest {
         editedLesson = new LessonBuilder(LESSON_FRI_8_TO_10)
                 .withNumberOfOccurrences(VALID_NUMBER_OF_OCCURRENCES_7_LESSON_WED_2_TO_4).build();
         assertTrue(LESSON_FRI_8_TO_10.isSameLesson(editedLesson));
+
+        // different attendance record list -> return true
+        editedLesson = new LessonBuilder(LESSON_FRI_8_TO_10)
+                .withAttendanceRecordList(new AttendanceRecordList(VALID_NUMBER_OF_OCCURRENCES_7_LESSON_WED_2_TO_4))
+                .build();
+        assertTrue(LESSON_FRI_8_TO_10.isSameLesson(editedLesson));
+    }
+
+    @Test
+    public void toString_returnsCorrectFormat() {
+        Lesson lesson = new Lesson(VALID_START_TIME, VALID_END_TIME, VALID_DAY_WED_LESSON_WED_2_TO_4,
+                VALID_NUMBER_OF_OCCURRENCES, VALID_VENUE, VALID_ATTENDANCE_RECORD_LIST);
+        String expectedString = (new StringBuilder()).append(VALID_DAY_WED_LESSON_WED_2_TO_4)
+                .append(" ")
+                .append(TIME_FORMATTER.format(VALID_START_TIME))
+                .append(" to ")
+                .append(TIME_FORMATTER.format(VALID_END_TIME))
+                .append(" Venue: ")
+                .append(VALID_VENUE.venue)
+                .append(" Number of occurrences: ")
+                .append(VALID_NUMBER_OF_OCCURRENCES.value).toString();
+        assertEquals(expectedString, lesson.toString());
     }
 
     @Test
@@ -94,6 +160,12 @@ public class LessonTest {
         // different number of occurrences -> returns false
         editedLesson = new LessonBuilder(LESSON_FRI_8_TO_10)
                 .withNumberOfOccurrences(VALID_NUMBER_OF_OCCURRENCES_7_LESSON_WED_2_TO_4).build();
+        assertFalse(LESSON_FRI_8_TO_10.equals(editedLesson));
+
+        // different attendance record list -> returns false
+        editedLesson = new LessonBuilder(LESSON_FRI_8_TO_10)
+                .withAttendanceRecordList(new AttendanceRecordList(VALID_NUMBER_OF_OCCURRENCES_7_LESSON_WED_2_TO_4))
+                .build();
         assertFalse(LESSON_FRI_8_TO_10.equals(editedLesson));
     }
 }
--- src/test/java/seedu/address/testutil/LessonBuilder.java
+++ src/test/java/seedu/address/testutil/LessonBuilder.java
@@ -4,6 +4,7 @@ import static seedu.address.model.lesson.Lesson.TIME_FORMATTER;
 
 import java.time.LocalTime;
 
+import seedu.address.model.attendance.AttendanceRecordList;
 import seedu.address.model.lesson.Day;
 import seedu.address.model.lesson.Lesson;
 import seedu.address.model.lesson.NumberOfOccurrences;
@@ -25,6 +26,7 @@ public class LessonBuilder {
     private Day day;
     private NumberOfOccurrences numberOfOccurrences;
     private Venue venue;
+    private AttendanceRecordList attendanceRecordList;
 
     /**
      * Creates a {@code LessonBuilder} with the default details.
@@ -35,6 +37,7 @@ public class LessonBuilder {
         day = DEFAULT_DAY;
         numberOfOccurrences = new NumberOfOccurrences(DEFAULT_NUMBER_OF_OCCURRENCES);
         venue = new Venue(DEFAULT_VENUE);
+        attendanceRecordList = new AttendanceRecordList(numberOfOccurrences.value);
     }
 
     /**
@@ -46,6 +49,7 @@ public class LessonBuilder {
         day = lessonToCopy.getDay();
         numberOfOccurrences = lessonToCopy.getNumberOfOccurrences();
         venue = lessonToCopy.getVenue();
+        attendanceRecordList = lessonToCopy.getAttendanceRecordList();
     }
 
     /**
@@ -88,7 +92,15 @@ public class LessonBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code attendanceRecordList} of the {@code Lesson} that we are building.
+     */
+    public LessonBuilder withAttendanceRecordList(AttendanceRecordList attendanceRecordList) {
+        this.attendanceRecordList = attendanceRecordList;
+        return this;
+    }
+
     public Lesson build() {
-        return new Lesson(startTime, endTime, day, numberOfOccurrences, venue);
+        return new Lesson(startTime, endTime, day, numberOfOccurrences, venue, attendanceRecordList);
     }
 }
