515d5e49f55ac1d1161830d0d9ece6a6486f2aef
ruixuantan <ruixuantan@yahoo.com.sg>
2020-10-21 10:55:39 +0800

    Add null parameter tests

--- src/test/java/tutorspet/logic/util/ModuleClassUtilTest.java
+++ src/test/java/tutorspet/logic/util/ModuleClassUtilTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static tutorspet.commons.core.Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX;
 import static tutorspet.commons.core.Messages.MESSAGE_INVALID_STUDENT_IN_MODULE_CLASS;
 import static tutorspet.logic.commands.AddLessonCommand.MESSAGE_EXISTING_LESSON;
+import static tutorspet.logic.commands.CommandTestUtil.VALID_ATTENDANCE;
 import static tutorspet.logic.commands.CommandTestUtil.VALID_PARTICIPATION_SCORE_80;
 import static tutorspet.logic.commands.CommandTestUtil.VALID_WEEK_1;
 import static tutorspet.logic.commands.CommandTestUtil.VALID_WEEK_5;
@@ -18,7 +19,9 @@ import static tutorspet.logic.util.ModuleClassUtil.deleteLessonFromModuleClass;
 import static tutorspet.logic.util.ModuleClassUtil.editAttendanceInModuleClass;
 import static tutorspet.logic.util.ModuleClassUtil.editLessonInModuleClass;
 import static tutorspet.logic.util.ModuleClassUtil.getAttendanceFromModuleClass;
+import static tutorspet.logic.util.ModuleClassUtil.getLessonFromModuleClass;
 import static tutorspet.testutil.Assert.assertThrows;
+import static tutorspet.testutil.TypicalIndexes.INDEX_FIRST_ITEM;
 import static tutorspet.testutil.TypicalModuleClass.CS2103T_TUTORIAL;
 import static tutorspet.testutil.TypicalStudent.ALICE;
 import static tutorspet.testutil.TypicalStudent.CARL;
@@ -38,7 +41,7 @@ import tutorspet.testutil.ModuleClassBuilder;
 
 public class ModuleClassUtilTest {
 
-    private static Lesson DEFAULT_LESSON = new LessonBuilder().build();
+    private static final Lesson DEFAULT_LESSON = new LessonBuilder().build();
 
     @Test
     public void addLessonToModuleClass_validParameters_success() throws CommandException {
@@ -57,6 +60,12 @@ public class ModuleClassUtilTest {
 
         assertThrows(CommandException.class, MESSAGE_EXISTING_LESSON, () ->
                 addLessonToModuleClass(CS2103T_TUTORIAL, lessonToAdd));
+    }
+
+    @Test
+    public void addLessonToModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(CommandException.class, () -> addLessonToModuleClass(null, DEFAULT_LESSON));
+        assertThrows(CommandException.class, () -> addLessonToModuleClass(CS2103T_TUTORIAL, null));
     }
 
     @Test
@@ -85,6 +94,18 @@ public class ModuleClassUtilTest {
     }
 
     @Test
+    public void editLessonInModuleClass_nullParameters_throwsNullPointerException() {
+        Lesson lessonToEdit = CS2103T_TUTORIAL.getLessons().get(0);
+
+        assertThrows(NullPointerException.class, () -> editLessonInModuleClass(
+                null, lessonToEdit, DEFAULT_LESSON));
+        assertThrows(NullPointerException.class, () -> editLessonInModuleClass(
+                CS2103T_TUTORIAL, null, DEFAULT_LESSON));
+        assertThrows(NullPointerException.class, () -> editLessonInModuleClass(
+                CS2103T_TUTORIAL, lessonToEdit, null));
+    }
+
+    @Test
     public void deleteLessonFromModuleClass_validParameters_success() throws CommandException {
         List<Lesson> updatedLessons = new ArrayList<>(CS2103T_TUTORIAL.getLessons());
         Lesson lessonToDelete = updatedLessons.get(0);
@@ -97,8 +118,28 @@ public class ModuleClassUtilTest {
     }
 
     @Test
-    public void deleteLessonFromModuleClass_nullModuleClass_throwsNullPointerException() {
+    public void deleteLessonFromModuleClass_nullParameters_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> deleteLessonFromModuleClass(null, DEFAULT_LESSON));
+        assertThrows(NullPointerException.class, () -> deleteLessonFromModuleClass(CS2103T_TUTORIAL, null));
+    }
+
+    @Test
+    public void getLessonFromModuleClass_validParameters_success() throws CommandException {
+        Lesson expectedLesson = CS2103T_TUTORIAL.getLessons().get(INDEX_FIRST_ITEM.getZeroBased());
+        assertEquals(expectedLesson, getLessonFromModuleClass(CS2103T_TUTORIAL, INDEX_FIRST_ITEM));
+    }
+
+    @Test
+    public void getLessonFromModuleClass_invalidWeekIndex_throwsCommandException() {
+        Index invalidIndex = Index.fromZeroBased(CS2103T_TUTORIAL.getLessons().size());
+        assertThrows(CommandException.class, MESSAGE_INVALID_LESSON_DISPLAYED_INDEX, () ->
+                getLessonFromModuleClass(CS2103T_TUTORIAL, invalidIndex));
+    }
+
+    @Test
+    public void getLessonFromModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> getLessonFromModuleClass(null, INDEX_FIRST_ITEM));
+        assertThrows(NullPointerException.class, () -> getLessonFromModuleClass(CS2103T_TUTORIAL, null));
     }
 
     @Test
@@ -134,6 +175,20 @@ public class ModuleClassUtilTest {
     }
 
     @Test
+    public void addAttendanceToModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> addAttendanceToModuleClass(
+                null, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> addAttendanceToModuleClass(
+                CS2103T_TUTORIAL, null, VALID_WEEK_1, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> addAttendanceToModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, null, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> addAttendanceToModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, null, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> addAttendanceToModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE, null));
+    }
+
+    @Test
     public void editAttendanceInModuleClass_validParameters_success() throws CommandException {
         Attendance attendanceToSet = new Attendance(VALID_PARTICIPATION_SCORE_80);
         Index lessonIndex = Index.fromZeroBased(0);
@@ -166,6 +221,20 @@ public class ModuleClassUtilTest {
     }
 
     @Test
+    public void editAttendanceInModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> editAttendanceInModuleClass(
+                null, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> editAttendanceInModuleClass(
+                CS2103T_TUTORIAL, null, VALID_WEEK_1, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> editAttendanceInModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, null, ALICE, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> editAttendanceInModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, null, VALID_ATTENDANCE));
+        assertThrows(NullPointerException.class, () -> editAttendanceInModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE, null));
+    }
+
+    @Test
     public void deleteAttendanceFromModuleClass_validParameters_success() throws CommandException {
         Index lessonIndex = Index.fromZeroBased(0);
 
@@ -195,6 +264,18 @@ public class ModuleClassUtilTest {
     }
 
     @Test
+    public void deleteAttendanceFromModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> deleteAttendanceFromModuleClass(
+                null, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE));
+        assertThrows(NullPointerException.class, () -> deleteAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, null, VALID_WEEK_1, ALICE));
+        assertThrows(NullPointerException.class, () -> deleteAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, null, ALICE));
+        assertThrows(NullPointerException.class, () -> deleteAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, null));
+    }
+
+    @Test
     public void getAttendanceFromModuleClass_validParameters_success() throws CommandException {
         Index lessonIndex = Index.fromZeroBased(0);
 
@@ -216,5 +297,17 @@ public class ModuleClassUtilTest {
 
         assertThrows(CommandException.class, MESSAGE_INVALID_LESSON_DISPLAYED_INDEX, () ->
                 getAttendanceFromModuleClass(CS2103T_TUTORIAL, lessonIndex, VALID_WEEK_1, ALICE));
+    }
+
+    @Test
+    public void getAttendanceFromModuleClass_nullParameters_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> getAttendanceFromModuleClass(
+                null, INDEX_FIRST_ITEM, VALID_WEEK_1, ALICE));
+        assertThrows(NullPointerException.class, () -> getAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, null, VALID_WEEK_1, ALICE));
+        assertThrows(NullPointerException.class, () -> getAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, null, ALICE));
+        assertThrows(NullPointerException.class, () -> getAttendanceFromModuleClass(
+                CS2103T_TUTORIAL, INDEX_FIRST_ITEM, VALID_WEEK_1, null));
     }
 }
