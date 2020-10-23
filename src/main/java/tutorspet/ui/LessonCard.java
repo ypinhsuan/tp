8e98a4007ea2ee17be96246693312059f88587f2
ypinhsuan <yenpinhsuan@gmail.com>
2020-10-23 20:22:50 +0800

    Add javadocs

--- src/main/java/tutorspet/ui/LessonCard.java
+++ src/main/java/tutorspet/ui/LessonCard.java
@@ -8,6 +8,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import tutorspet.model.lesson.Lesson;
 
+/**
+ * A UI component that displays information of a {@code Lesson}.
+ */
 public class LessonCard extends UiPart<Region> {
 
     private static final String FXML = "LessonCard.fxml";
